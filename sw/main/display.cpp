e375b508bb83a72f489424cbd2cc970741bab7de
Josh Watts <josh+github@sroz.net>
2020-11-29 22:46:40 -0500

    WIP: display

--- sw/main/display.cpp
+++ sw/main/display.cpp
@@ -7,52 +7,64 @@
 
 extern "C" {
 #include "OLED/OLED_Driver.h"
-#include "UGUI/ugui.h"
 }
+
+#include "UGUICPP/uguicpp.h"
+#include "UGUICPP/window.h"
+
+#define MENU_TEST
+#ifdef MENU_TEST
+#include "UI/menu.h"
+Menu menu(8);
+void menu_test() {
+    menu.create();
+    menu.fullScreen();
+    menu.setTitleText("Title");
+    menu.setItem(0, "One");
+    menu.setItem(1, "Two");
+    menu.setItem(2, "Three");
+    menu.setItem(3, "Four");
+    menu.setItem(4, "Five");
+    menu.setItem(5, "Six");
+    menu.layout();
+    menu.show();
+}
+#else
+#define menu_test() (void)0
+#endif
+
+#define MAIN_TEST
+#ifdef MAIN_TEST
+#include "UI/main.h"
+Main main_window;
+void main_test() {
+    main_window.create();
+    main_window.fullScreen();
+    main_window.show();
+}
+#else
+#define main_test() (void)0
+#endif
+
 
 #include "display.h"
 
-static UG_GUI g;
 static const uint32_t guiUpdateTimeout = 10 / portTICK_PERIOD_MS;
 
-static void Display_setPixel(UG_S16 x,UG_S16 y, UG_COLOR c)
+Display *Display::_instance = nullptr;
+
+Display *Display::getInstance(void)
 {
-    if (c != C_TRANSPARENT)
-        OLED_SetColor(x, y, c);
+    if (!_instance)
+        _instance = new Display();
+    return _instance;
 }
 
-Display::Display(void)
+Display::Display(void) :
+    Gui(Display::setPixel, OLED_X_MAXPIXEL, OLED_Y_MAXPIXEL)
 {
-    const int f_h = 26; // font height
-    const int w_s = 5; // widget space
-
-    this->_title.font = &FONT_16X26;
-    this->_title.pos = {
-        0,
-        5,
-        127,
-        5 + f_h + 2,
-    };
-    this->_title.padding = { 2, 5, 0, 5 };
-    this->_title.textColor = 4;
-    this->_title.fillColor = 0;
-    this->_title.rotateDelay_ms = 750;
-    this->_title.initialDelay_ms = 1500;
-    this->_widgets.push_back(&this->_title);
-
-    this->_artist.font = &FONT_16X26;
-    this->_artist.pos = {
-        0,
-        this->_title.pos.y2 + w_s,
-        127,
-        this->_title.pos.y2 + w_s + f_h + 2,
-    };
-    this->_artist.padding = this->_title.padding;
-    this->_artist.textColor = 4;
-    this->_artist.fillColor = 0;
-    this->_title.rotateDelay_ms = 750;
-    this->_title.initialDelay_ms = 1500;
-    this->_widgets.push_back(&this->_artist);
+    this->g.desktop_color = 0;
+    this->resetDirty();
 }
 
 void Display::enable(void)
@@ -60,34 +72,83 @@ void Display::enable(void)
     OLED_Sys_Init();
     OLED_Init(SCAN_DIR_DFT);
 
-    UG_Init(&g, &Display_setPixel, OLED_X_MAXPIXEL, OLED_Y_MAXPIXEL);
+    OLED_Clear(0);
+    OLED_Display();
+
+    // TODO: Create UI elements
+    menu_test();
+    main_test();
 
     this->_nextGuiTick = 0;
 }
 
-void Display::loop(void)
+bool Display::loop(void)
 {
     if (this->_nextGuiTick <= xTaskGetTickCount()) {
+        Gui::update();
+
+        if (this->_dirty.xs < OLED_X_MAXPIXEL) {
+            OLED_DisWindow(
+                this->_dirty.xs,
+                this->_dirty.ys,
+                this->_dirty.xe,
+                this->_dirty.ye);
+            this->resetDirty();
+        }
+
         this->_nextGuiTick = xTaskGetTickCount() + guiUpdateTimeout;
-        this->update();
     }
+    return true;
 }
 
 void Display::setMediaInfo(MediaInfo &mi)
 {
-    this->_title.set(mi.getField(MediaInfo::FIELD_TITLE));
-    this->_artist.set(mi.getField(MediaInfo::FIELD_ARTIST));
+    // this->_title.set(mi.getField(MediaInfo::FIELD_TITLE));
+    // this->_artist.set(mi.getField(MediaInfo::FIELD_ARTIST));
 }
 
-void Display::update(void)
+void Display::inputEvent(Input::msg_t msg)
 {
-    OLED_Clear(0x0);
-    this->render();
-    OLED_Display();
+    UGUI::Window *w = UGUI::Window::getActive();
+    if (w)
+        static_cast<Window *>(w)->inputEvent(msg);
+    if (msg == Input::SELECT) {
+        if (w)
+            w->hide();
+        menu.show();
+    } else if (msg == Input::LONG_SELECT) {
+        if (w)
+            w->hide();
+        main_window.show();
+    }
 }
 
-void Display::render(void)
+#define C_TRANSPARENT 0xFF
+
+void Display::setPixel(UG_S16 x,UG_S16 y, UG_COLOR c)
 {
-    for (Widget *w : this->_widgets)
-        w->render();
+    Display *self = Display::getInstance();
+    //TODO: Instead of single dirty bit, expand area of redraw from {0,0}x{0,0}
+    if (c != C_TRANSPARENT) {
+        if (x < 0 || y < 0 || x >= OLED_X_MAXPIXEL || y >= OLED_Y_MAXPIXEL)
+            return;
+        OLED_SetColor(x, y, c);
+
+        if (x < self->_dirty.xs)
+            self->_dirty.xs = x;
+
+        if (x > self->_dirty.xe)
+            self->_dirty.xe = x;
+
+        if (y < self->_dirty.ys)
+            self->_dirty.ys = y;
+
+        if (y > self->_dirty.ye)
+            self->_dirty.ye = y;
+    }
+}
+
+void Display::resetDirty(void)
+{
+    _dirty = {OLED_X_MAXPIXEL, OLED_Y_MAXPIXEL, 0, 0};
 }
--- sw/main/display.h
+++ sw/main/display.h
@@ -2,27 +2,41 @@
 #define DISPLAY_H_
 
 #include <list>
+#include "input.h"
 #include "mediainfo.h"
-#include "textbox.h"
+#include "UGUICPP/uguicpp.h"
 
-class Display {
-public:
+#define RIGHT_TRIANGLE "\x10"
+#define LEFT_TRIANGLE "\x11"
+#define RIGHT_ARROW "\x1A"
+#define LEFT_ARROW "\x1B"
+#define RIGHT_ANGLE_QUOTE "\xAF"
+#define LEFT_ANGLE_QUOTE "\xAE"
+#define VERT_PIPE "\xB3"
+
+
+class Display : public UGUI::Gui {
+protected:
     Display();
-    ~Display() = default;
+
+public:
+    static Display *getInstance();
 
     void enable(void);
-    void loop(void);
+    bool loop(void);
 
     void setMediaInfo(MediaInfo &mi);
+    void inputEvent(Input::msg_t msg);
 
 private:
-    void update(void);
-    void render(void);
+    static void setPixel(UG_S16 x,UG_S16 y, UG_COLOR c);
+    void resetDirty(void);
 
+    static Display *_instance;
     uint32_t _nextGuiTick;
-    std::list<Widget*> _widgets;
-    TextBox _artist;
-    TextBox _title;
+    // TextBox _artist; // TODO: Move to MainWindow
+    // TextBox _title; // TODO: Move to MainWindow
+    UG_AREA _dirty;
 };
 
 #endif
